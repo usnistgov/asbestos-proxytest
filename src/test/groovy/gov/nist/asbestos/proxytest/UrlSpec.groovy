74402cdc33965dd336978252548aa4c226cd2e18
bill <bmajur@gmail.com>
2019-04-26 18:42:22 -0400

    Starting to debug ProxyServlet simid parsing

--- src/test/groovy/gov/nist/asbestos/proxytest/UrlSpec.groovy
+++ src/test/groovy/gov/nist/asbestos/proxytest/UrlSpec.groovy
@@ -1,10 +1,36 @@
 package gov.nist.asbestos.proxytest
 
 import spock.lang.Specification
-import groovyx.net.http.HTTPBuilder
 
 class UrlSpec extends Specification {
 
-    def uri = ''
-    def http = new HTTPBuilder(uri)
+
+    def 'foo' () {
+        when:
+        HttpURLConnection post = new URL('http://localhost:8080/fproxy_war').openConnection()
+        def message = '{"message":"this is a message"}'
+        post.setRequestMethod("POST")
+        post.setDoOutput(true)
+        post.setRequestProperty("Content-Type", "application/json")
+        post.getOutputStream().write(message.getBytes("UTF-8"))
+        def postRC = post.getResponseCode();
+
+        then:
+        postRC == 200
+    }
+
+
+    def 'bar' () {
+        when:
+        HttpURLConnection post = new URL('http://localhost:8888/sim/default__rr/reg/rb').openConnection()
+        def message = '{"message":"this is a message"}'
+        post.setRequestMethod("POST")
+        post.setDoOutput(true)
+        post.setRequestProperty("Content-Type", "application/json")
+        post.getOutputStream().write(message.getBytes("UTF-8"))
+        def postRC = post.getResponseCode();
+
+        then:
+        postRC == 200
+    }
 }
