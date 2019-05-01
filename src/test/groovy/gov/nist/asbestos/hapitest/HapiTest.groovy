d6217f6fda39d5858f6104a9e159c2c3bb7a4a10
bill <bmajur@gmail.com>
2019-05-01 10:19:59 -0400

    Working on event logging

--- src/test/groovy/gov/nist/asbestos/hapitest/HapiTest.groovy
+++ src/test/groovy/gov/nist/asbestos/hapitest/HapiTest.groovy
@@ -11,50 +11,63 @@ import spock.lang.Specification
 
 class HapiTest extends Specification {
     @Shared FhirContext ctx = FhirContext.forDstu3()
-    @Shared String serverBase = "http://localhost:8080/fhir"
-    @Shared IGenericClient client = ctx.newRestfulGenericClient(serverBase)
+    IGenericClient client
 
     def 'fhir create patient' () {
+        setup:
+        withBase("http://localhost:8080/fhir")
+
         when:
         Patient patient = new Patient();
-// ..populate the patient object..
+        // ..populate the patient object..
         patient.addIdentifier().setSystem("urn:system").setValue("12345");
         patient.addName().setFamily("Smith").addGiven("John");
-
-// Invoke the server create method (and send pretty-printed JSON
-// encoding to the server
-// instead of the default which is non-pretty printed XML)
-        MethodOutcome outcome = client.create()
-                .resource(patient)
-                .prettyPrint()
-                .encodedJson()
-                .execute();
-
-// The MethodOutcome object will contain information about the
-// response from the server, including the ID of the created
-// resource, the OperationOutcome response, etc. (assuming that
-// any of these things were provided by the server! They may not
-// always be)
-        IdDt id = (IdDt) outcome.getId();
-        System.out.println("Got ID: " + id.getValue());
+        def id = createPatient(patient)
 
         then:
         id
 
     }
 
-    def 'fhir test 2' () {
-        when:
-        Bundle results = client
-                .search()
-                .forResource(Patient.class)
-                .where(Patient.FAMILY.matches().value("duck"))
-                .returnBundle(Bundle)
-                .execute();
 
-        System.out.println("Found " + results.getEntry().size() + " patients named 'duck'");
+    def 'fhir create patient through proxy' () {
+        setup:
+        withBase('http://localhost:8080/fproxy_war/prox')
+
+        when:
+        Patient patient = new Patient();
+        // ..populate the patient object..
+        patient.addIdentifier().setSystem("urn:system").setValue("12345");
+        patient.addName().setFamily("Smith").addGiven("John");
+        def id = createPatient(patient)
 
         then:
-        true
+        id
+
     }
+
+    def withBase(String base) {
+        client = ctx.newRestfulGenericClient(base)
+    }
+
+    String createPatient(Patient patient) {
+        // Invoke the server create method (and send pretty-printed JSON
+        // encoding to the server
+        // instead of the default which is non-pretty printed XML)
+        MethodOutcome outcome = client.create()
+                .resource(patient)
+                .prettyPrint()
+                .encodedJson()
+                .execute();
+
+        // The MethodOutcome object will contain information about the
+        // response from the server, including the ID of the created
+        // resource, the OperationOutcome response, etc. (assuming that
+        // any of these things were provided by the server! They may not
+        // always be)
+        IdDt id = (IdDt) outcome.getId();
+        System.out.println("Got ID: " + id.getValue());
+        id.value
+    }
+
 }
