53eb1b5481f5e8e955f260989c3bc74c8ef228a6
bill <bmajur@gmail.com>
2019-05-09 08:53:22 -0400

    IT test pulling logs works

--- src/test/groovy/gov/nist/asbestos/hapitest/HapiTest.groovy
+++ src/test/groovy/gov/nist/asbestos/hapitest/HapiTest.groovy
@@ -1,10 +1,13 @@
 package gov.nist.asbestos.hapitest
 
 import ca.uhn.fhir.context.FhirContext
-import ca.uhn.fhir.model.primitive.IdDt
 import ca.uhn.fhir.rest.api.MethodOutcome
 import ca.uhn.fhir.rest.client.api.IGenericClient
+import gov.nist.asbestos.simapi.http.HttpDelete
+import gov.nist.asbestos.simapi.http.HttpGet
 import gov.nist.asbestos.simapi.http.HttpPost
+import gov.nist.asbestos.simapi.sim.basic.EventStoreItem
+import gov.nist.asbestos.simapi.sim.basic.EventStoreItemFactory
 import org.hl7.fhir.dstu3.model.Patient
 import org.hl7.fhir.instance.model.api.IIdType
 import spock.lang.Shared
@@ -14,37 +17,36 @@ class HapiTest extends Specification {
     @Shared FhirContext ctx = FhirContext.forDstu3()
     IGenericClient client
 
-    def 'fhir create patient' () {
-        setup:
-        withBase("http://localhost:8080/fhir")
+    def 'fhir create patient through proxy' () {
+        setup:  // create channel
+        deleteChannel()
+        withBase("http://localhost:8081/fproxy_war/prox/${createChannel('default', 'fhirpass')}/Channel")
 
-        when:
+        when: // submit patient resource
         Patient patient = new Patient();
         // ..populate the patient object..
         patient.addIdentifier().setSystem("urn:system").setValue("12345");
         patient.addName().setFamily("Smith").addGiven("John");
         def id = createPatient(patient)
 
-        then:
+        then:  // if successful then id assigned
         id
 
+        when: // pull logs
+        HttpGet getter = new HttpGet()
+        getter.getJson("http://localhost:8081/fproxy_war/prox/default__fhirpass/Event")
+
+        then:
+        getter.status == 200
+        List<EventStoreItem> items = EventStoreItemFactory.parse(getter.responseText)
+        items.size() == 2
+        // in alphabetical order
+        items[0].resource == 'Patient'
+        items[1].resource == 'metadata'
     }
 
-
-    def 'fhir create patient through proxy' () {
-        setup:
-        withBase("http://localhost:8081/fproxy_war/prox/${createChannel('default', 'fhirpass')}")
-
-        when:
-        Patient patient = new Patient();
-        // ..populate the patient object..
-        patient.addIdentifier().setSystem("urn:system").setValue("12345");
-        patient.addName().setFamily("Smith").addGiven("John");
-        def id = createPatient(patient)
-
-        then:
-        id
-
+    void deleteChannel() {
+        new HttpDelete().run("http://localhost:8081/fproxy_war/prox/default__fhirpass")
     }
 
     String createChannel(String testSession, String id) {
