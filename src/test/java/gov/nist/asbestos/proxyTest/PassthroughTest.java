fa1d4585cdbca42a5a5b3e6a7a488e1896f57329
bill <bmajur@gmail.com>
2019-06-03 10:21:54 -0400

    Create/delete/get channel works again.

--- src/test/java/gov/nist/asbestos/proxyTest/PassthroughTest.java
+++ src/test/java/gov/nist/asbestos/proxyTest/PassthroughTest.java
@@ -1,11 +1,10 @@
 package gov.nist.asbestos.proxyTest;
 
-import ca.uhn.fhir.context.FhirContext;
-import ca.uhn.fhir.rest.client.api.IGenericClient;
+import gov.nist.asbestos.http.operations.HttpDelete;
+import gov.nist.asbestos.http.operations.HttpGet;
 import gov.nist.asbestos.http.operations.HttpPost;
 import gov.nist.asbestos.sharedObjects.ChannelConfig;
 import gov.nist.asbestos.sharedObjects.ChannelConfigFactory;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -16,16 +15,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PassthroughTest {
 
-//    private FhirContext ctx;
-//    private IGenericClient client;
-//
-//    @BeforeAll
-//    void beforeAll() {
-//        ctx = FhirContext.forR4();
-//    }
-
-    @Test
+   @Test
     void createAChannel() throws URISyntaxException, IOException {
+
+       // create
         ChannelConfig channelConfig = new ChannelConfig()
                 .setTestSession("default")
                 .setChannelId("test")
@@ -35,7 +28,24 @@ class PassthroughTest {
                 .setFhirBase("http://localhost:8080/fhir/fhir");
         String json = ChannelConfigFactory.convert(channelConfig);
         HttpPost poster = new HttpPost();
-        poster.postJson(new URI("http://localhost:8081/asbestos_proxy_war_exploded/prox"), json);
+        poster.postJson(new URI("http://localhost:8081/proxy/prox"), json);
         assertEquals(200, poster.getStatus());
+
+        // verify
+        HttpGet getter = new HttpGet();
+        getter.getJson(new URI("http://localhost:8081/proxy/prox/default__test"));
+        assertEquals(200, getter.getStatus());
+        ChannelConfig returnConfig = ChannelConfigFactory.convert(getter.getResponseText());
+        assertEquals(channelConfig, returnConfig);
+
+        // delete
+       HttpDelete deleter = new HttpDelete();
+       deleter.run(new URI("http://localhost:8081/proxy/prox/default__test"));
+       assertEquals(200, deleter.getStatus());
+
+       // verify
+       getter = new HttpGet();
+       getter.getJson(new URI("http://localhost:8081/proxy/prox/default__test"));
+       assertEquals(404, getter.getStatus());
     }
 }
